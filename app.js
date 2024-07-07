b0f03f46602280a0a43b0d3008f99c4920556b33
EkfrazoMohammed <mohammed@ekfrazo.in>
2024-07-07 17:16:57 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,105 +1,56 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
-const { StaticRouter } = require("react-router-dom/server");
+const { StaticRouter } = require("react-router-dom");
 const { HelmetProvider } = require("react-helmet-async");
-const fs = require("fs");
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(logger("dev"));
+// Middleware and static files setup
+app.use(express.static(path.join(__dirname, "build"))); // Serve static assets from "build" folder
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-// Serve static assets from the "build" directory
-app.use(express.static(path.join(__dirname, "build")));
-
+// SSR route for all routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-// SSR route
-app.get("/*", (req, res) => {
   const helmetContext = {};
-
-  const appStream = ReactDOMServer.renderToPipeableStream(
-    React.createElement(
-      HelmetProvider,
-      { context: helmetContext },
-      React.createElement(
-        StaticRouter,
-        { location: req.url },
-        React.createElement(App)
-      )
-    ),
-    {
-      bootstrapScripts: ["/static/js/main.js"], // Adjust the path if necessary
-      onAllReady() {
-        const { helmet } = helmetContext;
-
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-
-        // Read the template
-        const indexFile = path.resolve("./build/index.html");
-        fs.readFile(indexFile, "utf8", (err, data) => {
-          if (err) {
-            console.error("Could not read index.html file:", err);
-            res.status(500).send("Internal server error");
-            return;
-          }
-
-          // Insert the rendered app into the template
-          const html = data
-            .replace(
-              '<div id="root"></div>',
-              `<div id="root">${appStream}</div>`
-            )
-            .replace(
-              "</head>",
-              `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}</head>`
-            );
-
-          res.send(html);
-        });
-      },
-      onShellError() {
-        res.statusCode = 500;
-        res.send("<!doctype html><p>Loading...</p>");
-      },
-    }
+  const appStream = ReactDOMServer.renderToNodeStream(
+    <HelmetProvider context={helmetContext}>
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    </HelmetProvider>
   );
+
+  // Read the index.html file
+  const indexFile = path.resolve("./build/index.html");
+  fs.readFile(indexFile, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading index.html:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    // Insert rendered app stream and Helmet contents into the HTML template
+    const html = data
+      .replace('<div id="root"></div>', `<div id="root">${appStream}</div>`)
+      .replace('</head>', `${helmetContext.helmet.title.toString()}${helmetContext.helmet.meta.toString()}${helmetContext.helmet.link.toString()}</head>`);
+
+    // Send the modified HTML to the client
+    res.status(200).send(html);
+  });
 });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
+// Error handling middleware
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", {
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
 });
 
 app.listen(() => {
   console.log(`Server is running`);
-  
 });
-
-module.exports = app;
